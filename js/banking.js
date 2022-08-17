a0b4510f3751c5cc1a9d2f4c4a8ad0aec8fad816
Mahedi Hasan Bijoy <64260243+Mahedi2150@users.noreply.github.com>
2022-08-17 12:21:29 +0600

    update

--- js/banking.js
+++ js/banking.js
@@ -1,5 +1,5 @@
- // get deposit amounr
- // get withdraw amounr
+// get deposit amount
+// get withdraw amount 
 function getInput(inputFild) {
     const inputText = document.getElementById(inputFild);
     const inputAmount = parseFloat(inputText.value);
@@ -7,9 +7,9 @@ function getInput(inputFild) {
     return inputAmount;
 }
 
- // added deposit
- // get withdraw amount
-function updatePrevious(previousAmount,Amount) {
+// added deposit
+// get withdraw amount
+function updatePrevious(previousAmount, Amount) {
     const previousTotal = document.getElementById(previousAmount);
     const prevAmount = parseFloat(previousTotal.innerText);
     previousTotal.innerText = Amount + prevAmount;
@@ -22,12 +22,12 @@ function getCurrentBalance() {
     const prevBalanceAmount = parseFloat(previousBalance.innerText);
     return prevBalanceAmount
 }
-function updateBalance(amount,isAdd) {
+function updateBalance(amount, isAdd) {
     const previousBalance = document.getElementById("previousBalance");
     /*  const prevBalanceAmount = parseFloat(previousBalance.innerText); */
     const prevBalanceAmount = getCurrentBalance()
-    if (isAdd==true) {
-        
+    if (isAdd == true) {
+
         previousBalance.innerText = prevBalanceAmount + amount;
     } else {
         previousBalance.innerText = prevBalanceAmount - amount;
@@ -36,26 +36,26 @@ function updateBalance(amount,isAdd) {
 document.getElementById("depositButton").addEventListener("click", function () {
 
     const depositAmount = getInput("depositFild")
-    if (depositAmount>0) {
-        updatePrevious("previousDepo",depositAmount)
-        updateBalance(depositAmount,true)
-        
+    if (depositAmount > 0) {
+        updatePrevious("previousDepo", depositAmount)
+        updateBalance(depositAmount, true)
+
     }
     // get deposit amounr
     /* const deposit = document.getElementById("depositFild");
     const depositAmount =parseFloat(deposit.value); */
 
     // added deposit
-   /*  const previousDepo = document.getElementById("previousDepo");
-    const prevDepoAmount = parseFloat(previousDepo.innerText);
-    previousDepo.innerText = depositAmount + prevDepoAmount; */
+    /*  const previousDepo = document.getElementById("previousDepo");
+     const prevDepoAmount = parseFloat(previousDepo.innerText);
+     previousDepo.innerText = depositAmount + prevDepoAmount; */
     // clear input field 
     // deposit.value = "";
-// add total balance
-  /*   const previousBalance = document.getElementById("previousBalance");
-    const prevBalanceAmount = parseFloat(previousBalance.innerText);
-    previousBalance.innerText = prevBalanceAmount + depositAmount; */
-   
+    // add total balance
+    /*   const previousBalance = document.getElementById("previousBalance");
+      const prevBalanceAmount = parseFloat(previousBalance.innerText);
+      previousBalance.innerText = prevBalanceAmount + depositAmount; */
+
 })
 
 // ########################################
@@ -64,20 +64,20 @@ document.getElementById("depositButton").addEventListener("click", function () {
 document.getElementById("withdrawButton").addEventListener("click", function () {
     const currentBalance = getCurrentBalance()
     const withdrawAmount = getInput("withdrawFild")
-    if (withdrawAmount>0 && withdrawAmount<currentBalance) {
+    if (withdrawAmount > 0 && withdrawAmount < currentBalance) {
         updatePrevious("previousWithdraw", withdrawAmount)
-        updateBalance(withdrawAmount,false)
-        
+        updateBalance(withdrawAmount, false)
+
     }
     if (withdrawAmount > currentBalance) {
-        alert("You can't withdraw more than what you have in your account. Your current Balance is: "+currentBalance)
-       
+        alert("You can't withdraw more than what you have in your account. Your current Balance is: " + currentBalance)
+
     }
 
     // get withdraw amounr
-   /*  const withdraw = document.getElementById("withdrawFild");
-    const withdrawAmount = parseFloat(withdraw.value); */
-  
+    /*  const withdraw = document.getElementById("withdrawFild");
+     const withdrawAmount = parseFloat(withdraw.value); */
+
     // get withdraw amount
     /* const previouswithdraw = document.getElementById("previousWithdraw");
     const prevWithdrawAmount = parseFloat(previouswithdraw.innerText);
@@ -85,9 +85,9 @@ document.getElementById("withdrawButton").addEventListener("click", function () 
     // clear input field 
     // withdraw.value = "";
 
-// add total balance
+    // add total balance
     /* const previousBalance = document.getElementById("previousBalance");
     const prevBalanceAmount = parseFloat(previousBalance.innerText);
     previousBalance.innerText = prevBalanceAmount - withdrawAmount; */
-   
+
 })
